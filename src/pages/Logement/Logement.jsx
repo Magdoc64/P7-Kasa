956348958b47ccc662d825cd7d92607a0f9d21f6
Magdoc64 <magcvdrby@gmail.com>
2025-03-11 11:02:02 +0100

    correction formatting

--- src/pages/Logement/Logement.jsx
+++ src/pages/Logement/Logement.jsx
@@ -6,19 +6,12 @@ import Collapse from '../../components/Collapse.jsx'
 
 const Logement = () => {
     const {idCard} = useParams();
-
-    const currentLodge = lodges.filter(lodge => lodge.id === idCard);
-    const host = currentLodge[0].host;
-    const hostName = host.name;
-    const imageSrc = host.picture;
-    const descriptionDetail = [currentLodge[0].description];
-
-    const scaleValue = currentLodge[0].rating;
-    const range = [1,2,3,4,5];
-    const emptyValue = 5 - currentLodge[0].rating;
-
+    
     const testLodge = lodges.find(lodge => lodge.id === idCard);
+    const currentLodge = lodges.filter(lodge => lodge.id === idCard);
 
+    const range = [1,2,3,4,5];
+   
     return !testLodge ? (
         <main>
             <ErrorPage/>
@@ -41,21 +34,21 @@ const Logement = () => {
                     </div>
                     <div className="lodge-lieu-hote">
                         <div className="lodge-lieu-hote-id">
-                            <p className="lodge-lieu-hote-id-name">{hostName}</p>
-                            <img src={imageSrc} alt={hostName} className="lodge-lieu-hote-id-photo"/>
+                            <p className="lodge-lieu-hote-id-name">{currentLodge[0].host.name}</p>
+                            <img src={currentLodge[0].host.picture} alt={currentLodge[0].host.name} className="lodge-lieu-hote-id-photo"/>
                         </div>
                         <div className="lodge-lieu-hote-note">
-                            {range.map(rangeElement => scaleValue >= rangeElement? (
+                            {range.map(rangeElement => currentLodge[0].rating >= rangeElement? (
                                 <span key={rangeElement.toString()}><img src="../src/assets/star-color.png" alt="etoile pleine"/></span>
                             ):null)}
-                            {range.map(rangeElement => emptyValue >= range.indexOf(rangeElement)? (
+                            {range.map(rangeElement => (5 - currentLodge[0].rating) > range.indexOf(rangeElement)? (
                                 <span key={`${range.indexOf(rangeElement)}-${rangeElement.toString()}`}><img src="../src/assets/star-grey.png" alt="etoile vide"/></span>
                             ):null)}
                         </div>
                     </div>
                 </div>
                 <ul className="lodge-description">
-                    {<Collapse key={"description"} item="Description" description={descriptionDetail}/>}
+                    {<Collapse key={"description"} item="Description" description={[currentLodge[0].description]}/>}
                     {<Collapse key={"equipement"} item="Equipements" description={currentLodge[0].equipments}/>}
                 </ul>
             </section>
